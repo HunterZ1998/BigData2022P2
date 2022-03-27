bc677c11b3b8025533e2af7bf79265eccd9381c4
Yuchuan Huang <itisyuchuan@gmail.com>
2022-03-27 14:02:39 -0500

    fix stage with field delimiter

--- create_stage.sql
+++ create_stage.sql
@@ -1,5 +1,5 @@
 USE SCHEMA yesql_customers.raw;
 
-CREATE STAGE customers_s3_stage
+CREATE OR REPLACE STAGE customers_s3_stage
 URL = 's3://seng5709/customers/'
-FILE_FORMAT = ( TYPE = CSV );
+FILE_FORMAT = ( TYPE = CSV, FIELD_DELIMITER = '|');
